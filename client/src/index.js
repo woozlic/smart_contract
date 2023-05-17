3a9c487845244413800cdd04f570755b6dce47ca
Demin Dmitry Ni <dmitry.demin@megafon.ru>
2023-05-17 16:23:11 +0500

    up

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,45 @@
 import _ from 'lodash';
-function component() {
-  const element = document.createElement('div');
+// function component() {
+//   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// Lodash, currently included via a script, is required for this line to work
+// element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
+// return element;
+// }
+
+// document.body.appendChild(component());
+
+const contract_address = "0xa64f148865D0867110364928DFE7BdC18c9Dd02D";
+const data = require('../../contracts/artifacts/Storage_metadata.json');
+const Web3 = require('web3');
+const web3 = new Web3();
+
+const abi = data['output']['abi'];
+let contract;
+window.addEventListener('load', () => {
+    if (typeof (web3) === 'undefined') {
+        return console.log("Metamask is not installed");
+    }
+    contract = web3.eth.contract(abi).at(contract_address);
+    contract.message.call((error, result) => {
+        if (error) {
+            return console.log(error);
+        }
+        $('#message').text(result);
+    });
+});
+
+function setMessage() {
+    let message = $('#new_message').val();
+    contract.setMessage.sendTransaction(
+        message,
+        {gasPrice: web3.toWei(4.1, 'Gwei')},
+        (error, result) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log("txhash: " + result);
+        }
+    );
 }
-
-document.body.appendChild(component());
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {template} = require("lodash");
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [new HtmlWebpackPlugin()],
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }
+    )],
 };
